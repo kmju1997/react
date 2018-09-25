01b0d8b545bf5e82827950a8a0fab1f3ad93c905
kmju1997 <kmju1997@gmail.com>
2018-09-26 00:52:06 +0900

    calculator

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 "use strict"
 
-let time = new Date()
+function input() {
 
-let hour = time.getHours()
-let min = time.getMinutes()
-let second = time.getSeconds()
+    let tmp = document.getElementById("input").value;
+    tmp = tmp + (this.value);
+    document.getElementById("input").value = tmp;
 
-let currentTime = `time is now ${hour} : ${min} : ${second}`
-
-function buttonHandler(){
-
-    let time = new Date()
-
-    let hour = time.getHours()
-    let min = time.getMinutes()
-    let second = time.getSeconds()
-    
-    let currentTime = `time is now ${hour} : ${min} : ${second}`
-    
-    document.getElementById("content").innerHTML = currentTime
-    
 }
 
-document.getElementById("content").innerHTML = currentTime    
-document.getElementById("button").addEventListener("click", buttonHandler)
+function erase() {
+
+    if (this.id == "backspace") {
+
+        let tmp = document.getElementById("input").value; 
+        tmp = tmp.substr(0, tmp.length-1); 
+        document.getElementById("input").value = tmp;     
+    } 
+    
+    else {
+        document.getElementById("input").value = "";
+        document.getElementById("output").value = ""; 
+    }
+
+}
+
+
+function cal() {
+
+    let fomular = document.getElementById("input").value;
+    let result = eval(fomular);
+    document.getElementById("output").value = result;
+
+}
+
+let calbtn = document.getElementsByClassName("cal-btn");
+let erasebtn = document.getElementsByClassName("cal-erase");
+let equalbtn = document.getElementById("equal");
+
+for (let i = 0; i < calbtn.length; i++) {
+    calbtn[i].addEventListener('click', input);
+};
+
+for (let i = 0; i < erasebtn.length; i++) {
+    erasebtn[i].addEventListener('click', erase);
+};
+
+equalbtn.addEventListener('click', cal);
